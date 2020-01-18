2f854bea03c0f93936c57adde2842ae183a5cee9
MiccWan <michaelwan0906@hotmail.com>
2020-01-18 14:44:02 +0800

    change parameter "lineNum(int)" in function "navToCode" to "position(vscode.Position)"

--- dbux-code/src/codeControl/codeNav.js
+++ dbux-code/src/codeControl/codeNav.js
@@ -8,10 +8,15 @@ import vscode from 'vscode';
 
 const log = (...args) => console.log('[dbux-code][codeNav]', ...args)
 
-export function navToCode(URI, lineNum){
-  log(`Called navToCode with params { URI = ${URI}, lineNum = ${lineNum} }`)
+
+/**
+ * @param {vscode.Uri} URI (new vscode.Uri.file(FILEPATH))
+ * @param {vscode.position} position (new vscode.Position(LINE, CHARACTER))
+ */
+export function navToCode(URI, position){
+  log(`Called navToCode with params { URI = ${URI}, position = ${position} }`)
   vscode.window.showTextDocument(URI).then( editor => {
-    const range = editor.document.lineAt(lineNum).range;
+    const range = editor.document.lineAt(position.line).range;
     editor.selection =  new vscode.Selection(range.start, range.end);
     editor.revealRange(range);
   })
