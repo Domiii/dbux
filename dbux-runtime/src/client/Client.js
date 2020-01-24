4c704fda941769044f28ebd415164905b97ba46e
Domiii <dominik.seifert@gmail.com>
2020-01-25 02:42:13 +0800

    - kill runtime socket after x seconds of inactivity

--- dbux-runtime/src/client/Client.js
+++ dbux-runtime/src/client/Client.js
@@ -1,17 +1,24 @@
-import io from 'socket.io-client';
+import io, { Socket } from 'socket.io-client';
 import { newLogger, logInternalError } from 'dbux-common/src/log/logger';
 import SendQueue from './SendQueue';
 
 const { log, debug, warn, error: logError } = newLogger('CLIENT');
 
+// ########################################
+// config
+// ########################################
+
+const InactivityDelay = 5000;
 const DefaultPort = 3374;
-// const Remote = 'localhost';
-// const Remote = '127.0.0.1';
 const Remote = `ws://localhost:${DefaultPort}`;
 
 
 export default class Client {
+  /**
+   * @type {Socket}
+   */
   _socket;
+
   /**
    * @type {SendQueue}
    */
@@ -19,25 +26,7 @@ export default class Client {
   _connected = false;
 
   constructor() {
-    const socket = this._socket = io.connect(Remote, {
-      // jsonp: false,
-      // forceNode: true,
-      port: DefaultPort,
-      transports: ['websocket']
-    });
     this._sendQueue = new SendQueue(this);
-
-    // on reconnection, reset the transports option, as the Websocket
-    // connection may have failed (caused by proxy, firewall, browser, ...)
-    socket.on('reconnect_attempt', () => {
-      warn('reconnecting...');
-      // socket.io.opts.transports = ['websocket'];
-    });
-
-    socket.on('connect', this._handleConnect);
-    socket.on('connect_error', this._handleConnectFailed);
-    socket.on('disconnect', this._handleDisconnect);
-    socket.on('error', this._handleError);
   }
 
   isConnected() {
@@ -51,7 +40,12 @@ export default class Client {
   _handleConnect = () => {
     debug('connected');
     this._connected = true;
+
+    // send out anything that was already buffered
     this._sendQueue._flushLater();
+
+    // kill socket after configured period of inactivity
+    this._refreshInactivityTimer();
   };
 
   _handleConnectFailed = () => {
@@ -87,10 +81,57 @@ export default class Client {
    * @see https://github.com/socketio/engine.io-parser/blob/master/lib/index.js#L55
    */
   sendNow(data) {
+    if (!this._socket) {
+      this._connect();
+    }
     if (this.isConnected()) {
       this._socket.emit('data', data);
+      this._refreshInactivityTimer();
       return true;
     }
     return false;
+  }
+
+  // ###########################################################################
+  // connect
+  // ###########################################################################
+  
+  _connect() {
+    const socket = this._socket = io.connect(Remote, {
+      // jsonp: false,
+      // forceNode: true,
+      port: DefaultPort,
+      transports: ['websocket']
+    });
+
+    // on reconnection, reset the transports option, as the Websocket
+    // connection may have failed (caused by proxy, firewall, browser, ...)
+    socket.on('reconnect_attempt', () => {
+      warn('reconnecting...');
+      // socket.io.opts.transports = ['websocket'];
+    });
+
+    socket.on('connect', this._handleConnect);
+    socket.on('connect_error', this._handleConnectFailed);
+    socket.on('disconnect', this._handleDisconnect);
+    socket.on('error', this._handleError);
+  }
+
+  // ###########################################################################
+  // auto-disconnect regularly, so program won't run forever
+  // ###########################################################################
+
+  _refreshInactivityTimer() {
+    if (this._killTimer) {
+      clearTimeout(this._killTimer);
+    }
+    this._killTimer = setTimeout(this._disconnect, InactivityDelay);
+  }
+
+  _disconnect = () => {
+    if (this._socket) {
+      this._socket.disconnect();
+      this._socket = null;
+    }
   }
 }
