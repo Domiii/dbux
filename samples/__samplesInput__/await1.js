3b445912eab8c047c49908bd1f265a06e5fa254f
Domiii <dominik.seifert@gmail.com>
2020-02-03 01:45:38 +0800

    - minor updates; - fixed @babel/parser version again

--- samples/__samplesInput__/await1.js
+++ samples/__samplesInput__/await1.js
@@ -2,23 +2,22 @@ async function sleep(ms) {
   return new Promise(r => setTimeout(r, ms));
 }
 
-function f1() {
-  return sleep(100);
+function f1() { return sleep(2000); }
+
+async function f2() { await sleep(2000); }
+
+async function f3() { await sleep(2000); }
+
+async function f4(...fs) {
+  for (const f of fs) {
+    await f();
+    console.log('done!', f.name);
+  }
 }
 
-async function f2() {
-  await sleep(100);
-}
-
-async function f3() {
-  await sleep(200);
-}
-
-async function f4(...args) {
-  await Promise.all(args.map(f => f()));
-}
-
-(async function main() {
+async function main() {
   await f1();
-  await f4(f2, f3, sleep.bind(null, 400));
-})();
+  await f4(f2, f3, sleep.bind(null, 1000));
+}
+
+main();
