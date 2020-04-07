084a103a69d69c760b607b589d4545c987888ced
Domiii <dominik.seifert@gmail.com>
2020-04-07 19:16:39 +0800

    work on case studies (BugsJs + express)

---
+++ assets/projects/express/install.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -x    # trace mode
+
+source "../pre-install.sh"
+
+# config
+thisDir="$(getScriptDir "${BASH_SOURCE[0]}")"
+projectPath="express"
+githubUrl="https://github.com/BugsJS/express.git"
+bugId="26"
+checkoutWhat="test" # "test", "fix" or "full"
+
+
+# let's go!
+
+projectPathAbsolute="$ProjectsRoot/$projectPath)"
+projectRoot="$ProjectsRoot/$(getFirstInPath "$projectPath")"
+
+
+# clone
+cd $ProjectsRoot
+if [[ ! -e $projectRoot ]]; then
+  git clone $githubUrl
+fi
+
+cd $projectRoot
+
+# remove git folder
+if [[ -e "./.git" ]]; then
+  rm -rf "./.git"
+fi
+
+# checkout right branch
+git checkout "tags/Bug-$bugId-$checkoutWhat"
+
+
+# `# copy files (NOTE: Cygwin does not support glob-style copy)`
+# cp "$thisDir/files/package.json" .
+# cp "$thisDir/files/webpack.config.js" .
+# cp "$thisDir/files/nodemon.json" .
+
+# `# install default dependencies`
+# yarn install
+
+
+# `# install the good stuff`
+# yarn add --dev babel-loader @babel/node @babel/cli @babel/core @babel/preset-env && \
+# yarn add --dev webpack webpack-cli webpack-dev-server nodemon && \
+# yarn add core-js@3 @babel/runtime @babel/plugin-transform-runtime
+
+# `# run it!`
+# npm start
