8a8390095cd44082ac4ce08ddcedad4ba25a282f
Yggdrasill <del36031937@gmail.com>
2020-04-08 19:44:38 +0800

    add panzoom

--- dbux-graph-client/src/components/RunNode.js
+++ dbux-graph-client/src/components/RunNode.js
@@ -5,8 +5,10 @@ class RunNode extends ClientComponentEndpoint {
   createEl() {
     const el = compileHtmlElement(/*html*/`
       <div class="red">
-        <h6 data-el="title"></h6>
-        <button data-el="oCBtn" class="open_close_btn">▽</button>
+        <div style="display:flex; flex-direction:row;">
+          <h6 data-el="title"></h6>
+          <button data-el="oCBtn" class="open_close_btn">▽</button>
+        </div>
         <div data-mount="ContextNode" data-el="childrenContext"></div>
       </div>
     `);
