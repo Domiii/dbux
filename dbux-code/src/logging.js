b3849da6d78c8cb96acf6be3202ddf34e9195fef
MiccWan <michaelwan0906@hotmail.com>
2020-05-18 17:50:46 +0800

    - add toggleErrorLog command

--- dbux-code/src/logging.js
+++ dbux-code/src/logging.js
@@ -1,10 +1,23 @@
 import { window } from 'vscode';
 import { onLogError } from 'dbux-common/src/log/logger';
 
+let errorLogFlag = true;
+
+export function toggleErrorLog() {
+  setErrorLogFlag(!errorLogFlag);
+}
+
+export function setErrorLogFlag(val) {
+  errorLogFlag = !!val;
+  window.showInformationMessage(`${errorLogFlag ? 'showing' : 'hiding'} all error log.`);
+}
+
 export function initLogging() {
   onLogError(onError);
 }
 
 function onError(...args) {
-  window.showErrorMessage(args.join(' '));
+  if (errorLogFlag) {
+    window.showErrorMessage(args.join(' '));
+  }
 }
