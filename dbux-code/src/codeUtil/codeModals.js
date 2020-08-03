88157f79bfda7e09617a8c21a734cf81a3e94479
MiccWan <michaelwan0906@hotmail.com>
2020-08-03 16:46:04 +0800

    add button to show error log when logging error

--- dbux-code/src/codeUtil/codeModals.js
+++ dbux-code/src/codeUtil/codeModals.js
@@ -15,13 +15,16 @@ showInformationMessage(value, {
 ```
  */
 export async function showInformationMessage(message, btnConfig, messageCfg = EmptyObject) {
-  // suggest to open and use the first application that is selected and currently running.
   const result = await window.showInformationMessage(message, messageCfg, ...Object.keys(btnConfig));
   return await result && btnConfig[result]?.() || null;
 }
 
 export async function showWarningMessage(message, btnConfig, messageCfg = EmptyObject) {
-  // suggest to open and use the first application that is selected and currently running.
   const result = await window.showWarningMessage(message, messageCfg, ...Object.keys(btnConfig));
+  return await result && btnConfig[result]?.() || null;
+}
+
+export async function showErrorMessage(message, btnConfig, messageCfg = EmptyObject) {
+  const result = await window.showErrorMessage(message, messageCfg, ...Object.keys(btnConfig));
   return await result && btnConfig[result]?.() || null;
 }
