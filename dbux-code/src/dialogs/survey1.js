d7d8875fda74428cdb62e747b99c54c72084ba9b
Domiii <dominik.seifert@gmail.com>
2020-09-02 17:14:13 +0800

    survey

--- dbux-code/src/dialogs/survey1.js
+++ dbux-code/src/dialogs/survey1.js
@@ -21,17 +21,42 @@ async function serializeSurveyResult() {
   const tutorialResult = null;
 
   // TODO: get first bug result
-  
+  const bug1Status = null;
+
+  return {
+    installId,
+    surveyResult,
+    tutorialResult,
+    bug1Status
+  };
 }
+
+const whySurveyEdge = 
+{
+  text: 'Why these questions? What happens to my data?',
+  async click() {
+    const msg = `Dbux is the object of research for the doctoral dissertation of Dominik Seifert.
+If you agree to participating in this survey, we record your responses to these questions (and your progress on the tutorial bug) anonymously in order to evaluate Dbux's feasability and efficacy.
+
+We will not share this data with anyone, and no one has access to it (not even you, since it is anonymous).
+
+If you are concerned about your data or want your data to be deleted, just contact us on Discord.`;
+    const btns = {
+      async 'Contact us on Discord'() {
+        return env.openExternal(Uri.parse('https://discord.gg/jWN356W'));
+      },
+      async 'Help!'() {
+        return showHelp();
+      }
+    };
+    return showInformationMessage(msg, btns, { modal: true });
+  }
+};
 
 const survey1 = {
   name: 'survey1',
 
   defaultEdges: [
-    {
-      text: '(Continue Later)',
-      node: 'continueLater'
-    },
     function (currentState, stack, actions, node) {
       if (!node.nextNode) {
         return null;
@@ -40,6 +65,10 @@ const survey1 = {
         text: '(Skip this Question)',
         node: node.nextNode
       };
+    },
+    {
+      text: '(Continue Later)',
+      node: 'continueLater'
     },
     {
       text: '(Stop Survey)',
@@ -56,29 +85,12 @@ const survey1 = {
           text: 'Ok',
           node: nextNode
         },
-        {
-          text: 'Why? What happens to my data?',
-          async click() {
-            const msg = `Dbux is the object of research for the doctoral dissertation of Dominik Seifert.
-If you agree to participating in this survey, we record your responses to these questions (and your progress on the tutorial bug) anonymously under a randomly generated id, in order to help evaluate Dbux's feasability and efficacy, .
-
-If you are concerned about your data or want your data to be deleted, just contact us on Discord.`;
-            const btns = {
-              async 'Contact us on Discord'() {
-                return env.openExternal(Uri.parse('https://discord.gg/jWN356W'));
-              },
-              async 'Help!'() {
-                return showHelp();
-              }
-            };
-            return showInformationMessage(msg, btns, { modal: true });
-          }
-        },
+        whySurveyEdge
       ]
     },
 
     q1: {
-      nextnode: 'q2',
+      nextNode: 'q2',
       kind: DialogNodeKind.Modal,
       text: `Thank you for taking the time!
 How much do you agree with the following statement?
@@ -105,14 +117,18 @@ How much do you agree with the following statement?
     },
 
     q2: {
-      nextnode: 'q3',
+      nextNode: 'q3',
       kind: DialogNodeKind.Modal,
-      text: `[2/5] I believe, that if my debugger had some (or all) of these features, debugging could be *a lot* easier:
+      text: `[2/5] I believe, that if my debugger had some (or all) of these features, debugging could be a lot easier:
 
 - Navigation: Move forward+backward in time; skip non-function calls; jump to beginning/end of function
+
 - Values: Show all executions and result values of any piece of code
+
 - Object Tracking: Show all places where some object was used
+
 - Dynamic Call Graph: Show and allow exploring all of my program's execution paths
+
 - Decorate Executed Code in editor: So I can see what executed and what did not`,
       edges: [
         {
@@ -135,9 +151,9 @@ How much do you agree with the following statement?
     },
 
     q3: {
-      nextnode: 'q4',
+      nextNode: 'q4',
       kind: DialogNodeKind.Modal,
-      text: `Based on first impressions, I believe that Dbux can help me better understand how my programs work and what they do (given (1) I am familiar with Dbux and (2) I can properly integrate Dbux into my programs).`,
+      text: `[3/5] Based on first impressions, I believe that Dbux can help me better understand how my programs work and what they do (given (1) I am familiar with Dbux and (2) I can properly integrate Dbux into my programs).`,
       edges: [
         {
           text: 'Strongly Agree',
@@ -159,9 +175,9 @@ How much do you agree with the following statement?
     },
 
     q4: {
-      nextnode: 'q5',
+      nextNode: 'q5',
       kind: DialogNodeKind.Modal,
-      text: `What is your programming skill or background?`,
+      text: `[4/5] What is your programming skill or background?`,
       edges: [
         {
           text: 'Beginner Learner',
@@ -187,9 +203,9 @@ How much do you agree with the following statement?
     },
 
     q5: {
-      nextnode: 'interlude1',
+      nextNode: 'interlude1',
       kind: DialogNodeKind.Modal,
-      text: `I want to be more strategic and efficient when debugging. I am willing to invest time to learn and practice in order develop better strategies to find bugs faster.`,
+      text: `[5/5] Debugging is a crucial programming skill. I want to be more strategic and efficient when debugging. I am willing to invest time to learn and practice in order develop better strategies to find bugs faster.`,
       edges: [
         {
           text: 'Strongly Agree',
@@ -217,7 +233,11 @@ If you like, you can tell us a little about your opinion on Debugging in general
 Also, if you are interested in this project or in practicing debugging skills, feel free to leave your email.`,
       edges: [
         {
-          text: 'Give Opinion',
+          text: `I'm Done`,
+          node: 'end'
+        },
+        {
+          text: 'Let us know what you think',
           node: 'feedbackEnter'
         },
         {
@@ -225,9 +245,12 @@ Also, if you are interested in this project or in practicing debugging skills, f
           node: 'emailEnter'
         },
         {
-          text: `I'm Done`,
-          node: 'end'
-        }
+          text: 'Join Discord',
+          async click() {
+            return env.openExternal(Uri.parse('https://discord.gg/jWN356W'));
+          }
+        },
+        whySurveyEdge
       ]
     },
 
@@ -241,7 +264,9 @@ Also, if you are interested in this project or in practicing debugging skills, f
         });
       },
       text({ data }) {
-        return `yes? (${data.email || ''})`;
+        return `Your feedback:
+
+${data.feedback || ''}`;
       },
       edges: [
         {
@@ -261,15 +286,17 @@ Also, if you are interested in this project or in practicing debugging skills, f
         data.email = await window.showInputBox({
           placeHolder: 'Your email',
           value: data.email || '',
-          prompt: 'Enter email (we will hopefully send updates every few weeks)'
+          prompt: 'Enter email to stay updated on Dbux (we will hopefully have updates every few weeks)'
         });
       },
       text({ data }) {
-        return `Is this your email? (${data.email || ''})`;
+        return `Your email:
+
+${data.email || ''}`;
       },
       edges: [
         {
-          text: `Yes`,
+          text: `Ok`,
           node: 'interlude1'
         },
         {
@@ -296,7 +323,8 @@ Also, if you are interested in this project or in practicing debugging skills, f
           {
             text: `Ok`,
             node: previousState.nodeName
-          }
+          },
+          whySurveyEdge
         ];
       }
     },
@@ -311,12 +339,13 @@ Also, if you are interested in this project or in practicing debugging skills, f
       text: 'Thank you for trying out our survey! (Btw: You can press ESC to close this message)',
       edges: [
         {
-          text: 'I want to see the data that is being stored!',
+          text: 'I want to see the recorded data',
           async click(currentState, stack, { getRecordedData }) {
             const recordedData = getRecordedData();
             return renderValueAsJsonInEditor(recordedData);
           }
-        }
+        },
+        whySurveyEdge
       ]
     }
   },
