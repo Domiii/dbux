b4bc37d6a78885371e3fe67e61fb89141a08d9f8
Domiii <Domiii@users.noreply.github.com>
2020-12-03 23:42:56 +0800

    add some first dbux-experiments in order to use dbux to debug itself

--- dbux-babel-plugin/.babelrc.js
+++ dbux-babel-plugin/.babelrc.js
@@ -1,6 +1,22 @@
+const cfg = require('../config/babel-presets-node');
+
+if (process.env.NODE_ENV === 'development') {
+  // big play experiments: use dbux to debug itself
+  let { plugins } = cfg;
+  try {
+    // if available (and if experimenting), add (a separate copy of the) babel plugin to itself
+    const self = require('../../dbux-experiments/node_modules/@dbux/babel-plugin');
+    plugins = plugins.concat(self);
+  }
+  catch (err) {
+    // don't do anything
+  }
+}
+
 module.exports = {
   ignore: ['node_modules'],
   "sourceMaps": "inline",
   "retainLines": true,
-  ...require('../config/babel-presets-node')
+  ...cfg,
+  plugins
 };
