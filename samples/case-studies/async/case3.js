0ac30efb0a36e6c7fd14b6d392c63922fd19a3be
MiccWan <michaelwan0906@gmail.com>
2020-12-17 15:42:09 +0800

    add new sample and descriptions

--- samples/case-studies/async/case3.js
+++ samples/case-studies/async/case3.js
@@ -1,4 +1,8 @@
-const p = new Promise(r => {r()});
+/**
+ * @file The Promise p is both `awaited` and `thened`, 
+ */
+
+const p = new Promise(r => { r() });
 
 async function f() {
   await p;
@@ -6,4 +10,4 @@ async function f() {
 
 f();
 
-p.then(() => {})
+p.then(() => { })
---
+++ samples/case-studies/async/case5.js
@@ -0,0 +1,37 @@
+/**
+ * @file For `Promise.then` cases, sometimes we want to link the context that creates promise as parent, but sometimes we want to link the executor context as parent.
+ */
+
+function g() {
+  console.log('g');
+}
+
+function h() {
+  console.log('h');
+}
+
+/**
+ * case 1: We expect `delayG` to be the parent of `h`, since promise is created by lib(i.e. `delay`), we can't find the executor context
+ */
+
+import { delay } from 'sampleUtil';
+
+function main() {
+  return delayG().then(h);
+}
+
+function delayG() {
+  return delay(g, 200);
+}
+
+main();
+
+/**
+ * case 2: We expect `g` to be the parent of `h`
+ */
+
+function main2() {
+  return new Promise((r) => setTimeout(r, 200)).then(g).then(h)
+}
+
+main(2);
