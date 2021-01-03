6255ff05c07a139659843d8850633bad0738849b
Domiii <Domiii@users.noreply.github.com>
2021-01-04 05:38:28 +0800

    more async experiments

--- samples/case-studies/async/async_examples_mix.js
+++ samples/case-studies/async/async_examples_mix.js
@@ -4,37 +4,39 @@ import { f, g, f1, f2, f3, g1, g2 } from './_common';
 
 // Ex. 1: f and g execute in parallel threads
 (async function () {
-  f();
-  g();
+  f(1);                  // F(0, 1)
+  f(2);                  // 
 })();
 
 // Ex. 2: all runs f and g execute in serial, in the same thread
 (async function () {
-  await f();
-  await g();
+  await f(1);
+  await f(2);
 })();
 
 // Ex. 3: equivalent to Ex. 2
 (async function () {
-  const pf = f();
+  const pf = f(1);
   await pf;
-  const pg = g();
-  await pg();
+  const pg = f(2);
+  await pg;
 })();
 
 // Ex. 4: equivalent to Ex. 2, 3
 (async function () {
-  const p = f();
-  await p.then(g);
+  const p = f(1);
+  await p.then(() => f(2));
 })();
 
 // Ex. 5: equivalent to Ex. 2, 3, 4
+//        (but with a return value)
 (function () {
-  const p = f();
-  return p.then(g);
+  const p = f(1);
+  return p.then(() => f(2));
 })();
 
-// Ex. 6: equivalent to Ex. 2, 3, 4, 5 (but with an additional function call)
+// Ex. 6: equivalent to Ex. 2, 3, 4, 5
+//        (but with an additional function call)
 (function () {
   async function ff() {
     await f();
@@ -47,21 +49,21 @@ import { f, g, f1, f2, f3, g1, g2 } from './_common';
 /**
  * Ex. 7:
  * 
- * A missing `await` in front of fff() leads to a forked thread for any
- * meaning f(1) and f(2) execute in parallel.
+ * Missing `await`s in front of ff() and fff() lead
+ * to all calls of f executing in parallel.
  */
 (async function () {
   async function ff() {
-    await f(1);
+    await f(1);           // F(0, 1)
   }
 
   async function fff() {
     ff();
-    await f(2);
+    await f(2);           // F(0, 2)
   }
 
   fff();
-  await f(3);
+  await f(3);             // C(0, 3)
 })();
 
 /**
@@ -74,25 +76,30 @@ import { f, g, f1, f2, f3, g1, g2 } from './_common';
  * Simple visualization of asynchronous graph:
  * 
  * 1
- * 2 -> 3
- * 4 -> 5
+ * 2 -- 3
+ * 4 -- 5
+ * 
+ * Changing `fff()` to `await fff()` would change the graph to:
+ *
+ * 1
+ * 2 -- 3 -- 4 -- 5
  */
 (async function () {
   async function ff() {
     f(1);
-    await f(2);
+    await f(2);           // C(0, 2)
   }
 
   async function fff() {
-    await ff();
-    await f(3);
+    await ff();           // [ignored]
+    await f(3);           // C(2, 3)
   }
 
   async function ffff() {
     fff();
-    await f(4);
+    await f(4);           // C(0, 4)
   }
 
-  await ffff();
-  await f(5);
+  await ffff();           // [ignored]
+  await f(5);             // C(4, 5)
 })();
