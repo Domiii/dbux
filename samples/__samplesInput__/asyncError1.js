a9e45264607ba5dc114fe5424a1fd7f31bb3d187
Domiii <Domiii@users.noreply.github.com>
2021-01-31 19:05:02 +0800

    merge

---
+++ samples/__samplesInput__/asyncError1.js
@@ -0,0 +1,25 @@
+/**
+ * Async errors will throw on all `await`s.
+ */
+
+var p;
+
+async function sleep(ms) {
+  return new Promise(r => setTimeout(r, ms));
+}
+
+async function f() {
+  return p = g();
+}
+
+async function f2() {
+  await p;
+}
+
+async function g() {
+  await sleep(100);
+  throw new Error('err');
+}
+
+f();
+f2();
