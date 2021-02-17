87b75036a411753e61ef5d2cc2fadb137141ff28
Domiii <Domiii@users.noreply.github.com>
2021-02-17 18:27:49 +0800

    added missing onlyOnce

--- samples/case-studies/async/asyncUtil.js
+++ samples/case-studies/async/asyncUtil.js
@@ -2,3 +2,24 @@
 export async function sleep(delay) {
   return new Promise(r => setTimeout(r, delay));
 }
+
+/**
+ * 
+ */
+export async function onlyOnce(asyncCb) {
+  // const promises = new Map();
+  let promise;
+
+  return async (...args) => {
+    if (args.length > 0) {
+      throw new Error(`onlyOnce currently does not support callbacks with parameters.`);
+    }
+    // let promise = promises.get(asyncCb);
+    if (!promise) {
+      // promises.set(asyncCb, promise = asyncCb(...args));
+      promise = asyncCb(...args);
+    }
+
+    return promise;
+  };
+}
