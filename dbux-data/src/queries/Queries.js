382c25bedba565fb0a5682f125f738e09a04855d
Domiii <Domiii@users.noreply.github.com>
2021-02-18 23:02:35 +0800

    fix several small recent bugs

--- dbux-data/src/queries/Queries.js
+++ dbux-data/src/queries/Queries.js
@@ -16,10 +16,11 @@ export default class Queries {
     this._queries[query.name] = query;
 
     query._init(dp);
-    this[query.name] = {
-      run: query.executor.bind(query, dp), // query all
-      // TODO: incremental queries
-      // TODO: query listeners
-    };
+    this[query.name] = query.executor.bind(query, dp);
+    // {
+    //   run: query.executor.bind(query, dp), // query all
+    //   // TODO: incremental queries
+    //   // TODO: query listeners
+    // };
   }
 }
