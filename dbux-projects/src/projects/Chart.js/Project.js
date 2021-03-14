15857a3faf13a5afaf6634e1bab0e4802854fc17
Domiii <Domiii@users.noreply.github.com>
2021-03-14 17:12:09 +0800

    added uniqueChildrenAmount to `RunNode`

--- dbux-projects/src/projects/Chart.js/Project.js
+++ dbux-projects/src/projects/Chart.js/Project.js
@@ -33,7 +33,17 @@ export default class ChartJsProject extends Project {
         description: 'Baseline: vertical bar graph.',
         runArgs: [],
         website: 'http://localhost:10001/samples/charts/bar/vertical.html'
-      }
+      },
+      // more bugs:
+      // {
+      // * easingFunction is not a function (v2 only?) - https://github.com/chartjs/Chart.js/issues/7180
+      // * maxTicksLimit does not work for gridlines when ticks are not displayed - https://github.com/chartjs/Chart.js/issues/7302
+      // }
+      // bugs not suited for learning:
+      // * Right-most point gets cut off in line chart: more of a layout bug or feature change - https://github.com/chartjs/Chart.js/issues/6414
+      //
+      // bugs that seem good but...:
+      // * only on ipad, iphone; also not reliably reproducible - https://github.com/chartjs/Chart.js/issues/6235
     ];
 
     return bugs.
