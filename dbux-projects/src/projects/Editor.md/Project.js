bad4a4dae82107aaea3f8f172a93aa63588149b9
Domiii <Domiii@users.noreply.github.com>
2021-04-16 23:35:08 +0800

    getting into the mood for Editor.md

---
+++ dbux-projects/src/projects/Editor.md/Project.js
@@ -0,0 +1,5 @@
+import Project from '../../projectLib/Project';
+
+export default class EditorMdProject extends Project {
+  // TODO: https://github.com/pandao/editor.md/blob/master/examples/full.html
+}
