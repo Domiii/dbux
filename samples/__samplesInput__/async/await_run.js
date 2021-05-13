a06e73b4c1ef512d15be3d93431f2d879645bb44
edisonhello <edisonhello@hotmail.com>
2021-05-13 17:01:31 +0800

    add log

--- samples/__samplesInput__/async/await_run.js
+++ samples/__samplesInput__/async/await_run.js
@@ -1,10 +1,23 @@
 
 async function g(x) {
+  console.log('g1')
   await 0;
+  console.log('g2')
 }
 
 async function f(x) {
+  console.log('f1')
   await g(x);
+  console.log('f2')
 }
 
+
+let waits = 0;
+waitOne().then(waitOne).then(waitOne).then(waitOne)
+
 f(1);
+
+function waitOne() {
+  console.log('wait', ++waits);
+  return Promise.resolve().then(()=>{});
+}
