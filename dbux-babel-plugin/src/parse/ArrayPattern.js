ab65d6e11c793b0e541104a0ff2bd8ec66863134
Domiii <Domiii@users.noreply.github.com>
2021-07-01 14:22:34 +0800

    renaming things

--- dbux-babel-plugin/src/parse/ArrayPattern.js
+++ dbux-babel-plugin/src/parse/ArrayPattern.js
@@ -9,11 +9,11 @@ import BaseNode from './BaseNode';
 export default class ArrayPattern extends BaseNode {
   static children = ['elements'];
 
-  /**
-   * @see https://github.com/babel/babel/blob/672a58660f0b15691c44582f1f3fdcdac0fa0d2f/packages/babel-traverse/src/scope/index.ts#L631
-   */
-  insertSlicedToArray() {
-    const args = [targetNode];
-    t.callExpression(this.hub.addHelper('slicedToArray'), args);
-  }
+  // /**
+  //  * @see https://github.com/babel/babel/blob/672a58660f0b15691c44582f1f3fdcdac0fa0d2f/packages/babel-traverse/src/scope/index.ts#L631
+  //  */
+  // insertSlicedToArray() {
+  //   const args = [targetNode];
+  //   t.callExpression(this.hub.addHelper('slicedToArray'), args);
+  // }
 }
