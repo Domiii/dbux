e4586817b046d2cb7763b29d86489801474668f0
MiccWan <michaelwan0906@hotmail.com>
2021-07-05 17:57:35 +0800

    handle `delete` operation in constructValueObjectShallow

---
+++ samples/__samplesInput__/slicing/modifyObject1.js
@@ -0,0 +1,17 @@
+/**
+ * @file reassignment/deletion of object properties
+ */
+
+let o = { a: 'oa' };
+
+console.log(o);
+
+o.b = 'ob';
+
+console.log(o);
+
+delete o.a;
+
+o.c = { a: 'oca' };
+
+console.log(o);
