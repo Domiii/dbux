12c6bd3b536f6ea4bef494096c66a4d210eab7c2
MiccWan <michaelwan0906@hotmail.com>
2021-08-12 16:26:10 +0800

    fix `initCommand` pass DataFlowController in wrong place

--- dbux-code/src/commands/index.js
+++ dbux-code/src/commands/index.js
@@ -1,20 +1,20 @@
 import { initTraceDetailsViewCommands } from './traceDetailsViewCommands';
 import { initUserCommands } from './userCommands';
 import { initApplicationsViewCommands } from './applicationsViewCommands';
-import { initCallGraphViewCommands } from './callGraphViewCommands';
+// import { initCallGraphViewCommands } from './callGraphViewCommands';
 import { initDataFlowViewCommands } from './dataFlowViewCommands';
 // import { initCallStackViewCommands } from './callStackViewCommands';
 
 export function initCommands(
   context,
   traceDetailsController,
-  callGraphViewController,
+  // callGraphViewController,
   dataFlowViewController
 ) {
   initUserCommands(context);
   initApplicationsViewCommands(context);
   initTraceDetailsViewCommands(context, traceDetailsController);
-  initCallGraphViewCommands(context, callGraphViewController);
-  initDataFlowViewCommands(context, dataFlowViewController)
+  // initCallGraphViewCommands(context, callGraphViewController);
+  initDataFlowViewCommands(context, dataFlowViewController);
   // initCallStackViewCommands(context, callStackViewController);
 }
