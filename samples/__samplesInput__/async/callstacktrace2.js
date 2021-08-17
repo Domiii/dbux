e0515ce5d4258e3df747354acda287a21b76ca68
Domiii <Domiii@users.noreply.github.com>
2021-08-17 18:08:13 +0800

    more work on call stack traces

---
+++ samples/__samplesInput__/async/callstacktrace2.js
@@ -0,0 +1,24 @@
+/**
+ * @file Async function async callstack.
+ * NOTE: in V8 (as of Summer 2021):
+ * * Error callstack would not go back up the promise chain.
+ * * reject does not have any callstack.
+ */
+
+Promise.resolve()
+  .then(() => { })
+  .then(() =>
+    Promise.resolve().then(() => {
+      throw new Error(123);
+    })
+  );
+
+setTimeout(() =>
+  Promise.resolve()
+    .then(() => { })
+    .then(() =>
+      Promise.resolve().then(() => {
+        Promise.reject(456);
+      })
+    )
+);
