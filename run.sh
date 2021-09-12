58a0ae3dad5baf98c25d92408ef3a0d6aab2fc48
Domiii <Domiii@users.noreply.github.com>
2021-09-12 21:59:39 +0800

    fix multiple CHAINs from same root

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@
 set -e # cancel on error
 # set -x # verbose echo mode
 
-fname="__samplesInput__/finally1"
-# fname="case-studies/async/chain/chain1b"
+# fname="__samplesInput__/finally1"
+fname="case-studies/async/chain/chain2b"
 
 
 
