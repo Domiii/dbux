14fc241fd20a5a594171b7df349b8639691c41e6
Domiii <Domiii@users.noreply.github.com>
2021-11-07 22:03:26 +0800

    todomvc

--- dbux-projects/src/projects/todomvc-es6/Project.js
+++ dbux-projects/src/projects/todomvc-es6/Project.js
@@ -59,6 +59,7 @@ export default class TodomvcEs6Project extends Project {
         description: 'TODO items never show up. Luckily there is a clear error message.',
         patch: ['no-callbacks', 'error1'],
         domains: ['init', 'controller'],
+        stepsToReproduce: ['Have a non-empty list.'],
         bugTags: ['typo'],
         bugLocations: [
           {
@@ -71,11 +72,12 @@ export default class TodomvcEs6Project extends Project {
         }
       },
       {
+        // TODO: stepsToReproduce: 
         label: 'Completed TODO items are shown as not completed and vice versa (only during initial rendering)',
-        // description: 'TODO items never show up. Luckily there is a clear error message.',
         patch: ['no-callbacks', 'error2'],
         domains: ['init', 'render'],
-        bugTags: ['boolean logic', 'operator'],
+        stepsToReproduce: ['Have a non-empty list.', 'Make sure, some items are completed, some not.', 'Check whether they are rendered as "completed" correctly.'],
+        bugTags: ['boolean-logic', 'operator'],
         bugLocations: [
           {
             file: 'src/template.js',
@@ -88,9 +90,9 @@ export default class TodomvcEs6Project extends Project {
       },
       {
         label: 'Only the last TODO item is shown',
-        // description: 'TODO items never show up. Luckily there is a clear error message.',
         patch: ['no-callbacks', 'error3'],
-        domains: ['init', 'render'],
+        domains: ['render'],
+        stepsToReproduce: ['Have a list with 2, 3 or more items.'],
         bugTags: ['Array.reduce'],
         bugLocations: [
           {
@@ -104,10 +106,13 @@ export default class TodomvcEs6Project extends Project {
       },
       {
         label: 'Empty list, but no error message',
-        // description: 'TODO items never show up. Luckily there is a clear error message.',
         patch: ['no-callbacks', 'error4'],
-        domains: ['init', 'render'],
-        bugTags: ['typo'],
+        domains: ['render'],
+        stepsToReproduce: ['Have a non-empty list.'],
+        bugTags: [
+          'typo',
+          ['api', 'DOM'] // https://www.google.com/search?hl=en&q=mdn+dom
+        ],
         bugLocations: [
           {
             file: 'src/view.js',
@@ -119,180 +124,38 @@ export default class TodomvcEs6Project extends Project {
         }
       },
       {
-        label: '',
-        // description: 'TODO items never show up. Luckily there is a clear error message.',
+        label: '"X items left" is always plural, but should also support singular.',
         patch: ['no-callbacks', 'error5'],
-        domains: ['init', 'render'],
+        domains: ['render'],
+        stepsToReproduce: ['Have a non-empty list.', 'Make sure, exactly one item is not marked as "done" yet.'],
+        bugTags: ['wrong-condition'],
+        bugLocations: [
+          {
+            file: 'src/template.js',
+            line: 38
+          }
+        ]
+      },
+      {
+        label: '"',
+        patch: ['no-callbacks', 'error6'],
+        domains: ['render'],
+        stepsToReproduce: [],
         bugTags: [''],
         bugLocations: [
           {
             file: 'src/template.js',
-            line: 20
+            line: -1
           }
         ]
       },
-
-
       // {
-      //   label: 'Empty list with silenced exception',
-      //   patch: 'error3',
-      //   description: 'TODO items never show up. Sadly no error message is given. Luckily dbux displays an error indicator.',
-      //   runArgs: [],
-      //   bugLocations: [
-      //     {
-      //       file: 'src/controller.js',
-      //       line: 65
-      //     },
-      //     {
-      //       // empty catch statement
-      //       file: 'src/controller.js',
-      //       line: 244
-      //     }
-      //   ]
-      // },
-      // {
-      //   // for-loop, off-by-one
-      //   label: 'Last TODO item always missing',
-      //   patch: 'error6',
-      //   description: 'TODO list never renders the last item.',
-      //   runArgs: [],
-      //   bugLocations: [
-      //     {
-      //       file: 'src/template.js',
-      //       line: 65
-      //     }
-      //   ]
-      // },
-      // {
-      //   label: 'Empty list with symptomatic error message',
-      //   patch: 'error2',
-      //   description: 'TODO items never show up. We see an error message, but it is not the actual bug cause, only a symptom.',
-      //   runArgs: [],
-      //   bugLocations: [
-      //     {
-      //       file: 'src/controller.js',
-      //       line: 12
-      //     },
-      //     {
-      //       file: 'src/controller.js',
-      //       line: 13
-      //     }
-      //   ]
-      // },
-      // {
-      //   // ternary, css, branch logic reversed
-      //   label: 'Reversed strikethrough',
-      //   patch: 'error7',
-      //   description: 'Check and uncheck items to see that strikethrough logic for TODO items is incorrect.',
-      //   runArgs: [],
-      //   bugLocations: [
-      //     {
-      //       file: 'src/view.js',
-      //       line: 188
-      //     }
-      //   ]
-      // },
-      // {
-      //   // incorrect-variable,DOM
-      //   label: 'After editing, TODO text is incorrect',
-      //   patch: 'error9',
-      //   description: 'Edit any existing TODO, then save it (press ENTER).',
-      //   runArgs: [],
-      //   bugLocations: [
-      //     {
-      //       file: 'src/view.js',
-      //       line: 55
-      //     }
-      //   ]
-      // },
-      // {
-      //   // typo,variable,DOM
-      //   label: 'Incorrect "items left" amount',
-      //   patch: 'error8',
-      //   description: '1) Mark some items as done. 2) The "items left" amount is incorrect.',
-      //   runArgs: [],
-      //   bugLocations: [
-      //     {
-      //       file: 'src/model.js',
-      //       line: 106
-      //     }
-      //   ]
-      // },
-      // {
-      //   // for-loop,indexing
-      //   label: 'Changing existing TODOs does not persist anymore',
-      //   patch: 'error10',
-      //   description: '1) change name of TODO or check/uncheck a TODO. 2) Refresh page. 3) After refresh, TODO item renders incorrectly.',
-      //   runArgs: [],
-      //   bugLocations: [
-      //     {
-      //       file: 'src/store.js',
-      //       line: 100
-      //     }
-      //   ]
-      // },
-      // {
-      //   // omission,callbacks,wrong-arguments
-      //   label: 'Empty list #3',
-      //   patch: 'error11',
-      //   description: 'Again, TODO items just won\'t show up, but for yet another very different reason.',
-      //   runArgs: [],
-      //   bugLocations: [
-      //     {
-      //       file: 'src/model.js',
-      //       line: 51
-      //     },
-      //     {
-      //       file: 'src/model.js',
-      //       line: 52
-      //     }
-      //   ]
-      // },
-      // {
-      //   // for-loop,variable declaration,scope
-      //   label: 'Only last TODO item shows up',
-      //   patch: 'error5',
-      //   description: 'TODO list is not rendered completely if it contains more than one element.',
-      //   runArgs: [],
-      //   bugLocations: [
-      //     {
-      //       file: 'src/template.js',
-      //       line: 62
-      //     },
-      //     {
-      //       file: 'src/template.js',
-      //       line: 63
-      //     },
-      //     {
-      //       file: 'src/template.js',
-      //       line: 65
-      //     }
-      //   ]
-      // },
-      // {
-      //   label: 'Broken "Clear completed" button',
-      //   patch: 'error4',
-      //   description: '"Clear completed" button does not do anything. No error message.',
-      //   runArgs: [],
-      //   bugLocations: [
-      //     {
-      //       file: 'src/controller.js',
-      //       line: 164
-      //     }
-      //   ]
-      // },
-      // // more bugs:
-
-      // // template.show -> template has a minor render defect
-      // // template.show -> incorrect variable scope causes only one item to be rendered
-
-      {
-        // see: https://github.com/kentcdodds/es6-todomvc/issues/39
-        label: 'After switching between filters, the old filter still applies.',
-        // eslint-disable-next-line max-len
-        description: 'Add a todo item -> Go to "Completed" filter -> Reload the page -> Select "All" -> Mark item as completed. -> Observe bug: It won\'t show up anymore, even though we are in "All".',
-        runArgs: []
-      }
+      //   // see: https://github.com/kentcdodds/es6-todomvc/issues/39
+      //   label: 'After switching between filters, the old filter still applies.',
+      //   // eslint-disable-next-line max-len
+      //   description: 'Add a todo item -> Go to "Completed" filter -> Reload the page -> Select "All" -> Mark item as completed. -> Observe bug: It won\'t show up anymore, even though we are in "All".',
+      //   runArgs: []
+      // }
 
     ];
   }
