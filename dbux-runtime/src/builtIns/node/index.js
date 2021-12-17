52d0b542f4c34f898a76809858f055f72e75dc25
Domiii <Domiii@users.noreply.github.com>
2021-12-17 19:45:52 +0800

    fix: don't require `node`-related things in non-node runtime

---
+++ dbux-runtime/src/builtIns/node/index.js
@@ -0,0 +1,5 @@
+import patchNodeUtil from './node-util';
+
+export default function tryPatchNode() {
+  patchNodeUtil();
+}
--- dbux-runtime/src/builtIns/node/node-util.js
+++ dbux-runtime/src/builtIns/node/node-util.js
@@ -1,5 +1,5 @@
 import util from 'util';
-import { monkeyPatchFunctionHolderDefault } from '../util/monkeyPatchUtil';
+import { monkeyPatchFunctionHolderDefault } from '../../util/monkeyPatchUtil';
 // import { peekBCEMatchCallee } from '../data/dataUtil';
 
 // node -e "console.log(require('util'))"
@@ -13,8 +13,8 @@ export default function patchNodeUtil() {
    * so automatic builtin patcher won't patch this (since it modifies function arguments).
    * 
    * @see https://nodejs.org/docs/latest/api/util.html#util_util_inherits_constructor_superconstructor
-   * @deprecated
   */
+
   monkeyPatchFunctionHolderDefault(util, 'inherits');
   monkeyPatchFunctionHolderDefault(process, 'cwd');
 }
