6d8503bae15833363d12db671da48b9a74743855
Domiii <Domiii@users.noreply.github.com>
2022-01-02 20:37:03 +0800

    docs

--- docs_site/scripts/deploy_docs.sh
+++ docs_site/scripts/deploy_docs.sh
@@ -1,15 +1,24 @@
 set -e
 
-echo 'DOCS BUILDING...'
+branch=$(git symbolic-ref --short HEAD)
+
+if [ "$branch" != "master" ]; then
+  echo "You are on the wrong branch."
+  echo "  Expected=master"
+  echo "  Actual  =$branch"
+  exit -1
+fi
+
+# echo "You are on the right branch: $branch"
+echo "DOCS BUILDING..."
 
 yarn update-docs
 yarn build
 
-branch=$(git symbolic-ref --short HEAD)
 
 # TODO: get user input first
-echo Docs Built. You are on branch=$branch.
-echo Deploy? [y/N]
+echo "Docs Built. You are on branch=$branch."
+echo "Deploy? [y/N]"
 read ok
 
 if [ "$ok" != "y" ]; then
