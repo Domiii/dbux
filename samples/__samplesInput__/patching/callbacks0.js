e4965a73c7d618149ac0d1288f0c90145cdfa9d9
Domiii <Domiii@users.noreply.github.com>
2022-01-11 21:19:04 +0800

    WIP zoom ing in on #640

--- samples/__samplesInput__/patching/callbacks0.js
+++ samples/__samplesInput__/patching/callbacks0.js
@@ -1,7 +1,29 @@
+/**
+ * Test basic callback identity properties when dynamic callback patcher is involved.
+ */
+
 const assert = require('tools/assertCallbacks');
 
 function cb() {
   console.log('cb!');
+}
+
+var storedCb;
+
+/**
+ * dbux disable
+ */
+function nativeCallStore(cb) {
+  assert.patched(cb);
+  storedCb = cb;
+}
+
+/**
+ * dbux disable
+ */
+function nativeCallCompare(cb) {
+  assert.patched(cb);
+  console.assert(cb === storedCb, '❌ Same callback passed in twice -> should be the same.');
 }
 
 (async function main() {
@@ -9,6 +31,14 @@ function cb() {
 
   assert.not.patched(cb);
   assert.patched(Array.prototype.slice);
-  
+
+  /**
+   * Call function with same cb twice:
+   * -> it should be recognized as the same cb, or else `removeEventListener`
+   *    and many other callback management mechanisms cannot work correctly.
+   */
+  nativeCallStore(cb);
+  nativeCallCompare(cb);
+
   // TODO: more testing here, with checks for multi-patching
 })();
