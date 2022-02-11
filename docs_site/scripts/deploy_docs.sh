e00df2fe883d4d68ce2eca0b285c6e371241f671
Domiii <Domiii@users.noreply.github.com>
2022-02-11 21:51:03 +0800

    [deploy docs]

--- docs_site/scripts/deploy_docs.sh
+++ docs_site/scripts/deploy_docs.sh
@@ -1,25 +1,25 @@
 set -e
 
-branch=$(git symbolic-ref --short HEAD)
+# branch=$(git symbolic-ref --short HEAD)
 
-if [ "$branch" != "master" ]; then
-  echo "You are on the wrong branch: expected=master, actual=$branch"
-  exit -1
-fi
+# if [ "$branch" != "master" ]; then
+#   echo "You are on the wrong branch: expected=master, actual=$branch"
+#   exit -1
+# fi
 
-# echo "You are on the right branch: $branch"
-echo "DOCS BUILDING..."
+# # echo "You are on the right branch: $branch"
+# echo "DOCS BUILDING..."
 
-yarn update-docs
-yarn build
+# yarn update-docs
+# yarn build
 
 
-# TODO: get user input first
-echo "Docs Built. You are on branch=$branch."
-echo "Deploy? [Y/n]"
-read ok
+# # TODO: get user input first
+# echo "Docs Built. You are on branch=$branch."
+# echo "Deploy? [Y/n]"
+# read ok
 
-if [ "$ok" != "" & "$ok" != "y" ]; then
+if [ "$ok" != ""] & ["$ok" != "y" ]; then
   exit -1
 fi
 
