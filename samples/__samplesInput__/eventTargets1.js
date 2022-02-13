6d0bcc09d104bca3e342af73f8fb2ce272453da0
Domiii <Domiii@users.noreply.github.com>
2022-02-13 23:23:00 +0800

    EventTarget event handler identity kinda working

--- samples/__samplesInput__/eventTargets1.js
+++ samples/__samplesInput__/eventTargets1.js
@@ -1,9 +1,12 @@
+const evts = [];
 
-function cb(...args) {
-  console.log('event fired. args:', ...args)
+function cb1(evt) {
+  console.log('event fired:', evt);
+  evts.push(evt);
 }
-function cb2(...args) {
-  console.log('event fired. args:', ...args)
+function cb2(evt) {
+  console.log('event2 fired:', evt);
+  evts.push(evt);
 }
 
 (function main() {
@@ -15,14 +18,16 @@ function cb2(...args) {
     const evt1 = new Event('test', { x: 1 });
     eventTarget.dispatchEvent(evt1);
 
-    eventTarget.removeEventListener(cb1);
+    eventTarget.removeEventListener('test', cb1);
 
     const evt2 = new Event('test', { x: 2 });
     eventTarget.dispatchEvent(evt2);
 
-    eventTarget.removeEventListener(cb2);
+    eventTarget.removeEventListener('test', cb2);
 
     const evt3 = new Event('test', { x: 3 });
     eventTarget.dispatchEvent(evt3);
+    
+    console.log('Done! --', evts.length, '=== 3');
   });
 })();
