7fa813a9b10faf4fa4a523aa33130c559b5cc6e6
Domiii <Domiii@users.noreply.github.com>
2022-04-16 00:48:37 +0800

    samples

--- dbux-projects/assets/projectAssets/async-js/bug1.js
+++ dbux-projects/assets/projectAssets/async-js/bug1.js
@@ -3,14 +3,14 @@
  * @see https://github.com/caolan/async/issues/1729
  */
 
-const { default: sleep } = require('@dbux/common/src/util/sleep');
+function sleep(ms) { return new Promise(r => setTimeout(r, ms)); }
 const { queue } = require('./lib');
 
 function start() { }
 
 async function main() {
   await start();
-  const q = queue(async (task) => {
+  const q = queue(async function processTask(task) {
     await sleep(10);
     console.log("task done");
   });
