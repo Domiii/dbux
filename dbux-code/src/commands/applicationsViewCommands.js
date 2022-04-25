896556c2426b65b68206547f2fcdd1b4692860f7
MiccWan <michaelwan0906@gmail.com>
2022-04-25 18:26:41 +0800

    add `clear apps` button to ApplicationsView

--- dbux-code/src/commands/applicationsViewCommands.js
+++ dbux-code/src/commands/applicationsViewCommands.js
@@ -1,8 +1,10 @@
 import { newLogger } from '@dbux/common/src/log/logger';
+import allApplications from '@dbux/data/src/applications/allApplications';
 import { registerCommand } from './commandUtil';
 import { showTextDocument } from '../codeUtil/codeNav';
 import { initRuntimeServer, stopRuntimeServer } from '../net/SocketServer';
 import { emitShowApplicationEntryFileAction } from '../userEvents';
+import { getProjectManager } from '../projectViews/projectControl';
 
 // eslint-disable-next-line no-unused-vars
 const { log, debug, warn, error: logError } = newLogger('Commands');
@@ -16,6 +18,14 @@ export function initApplicationsViewCommands(context) {
   registerCommand(context,
     'dbux.stopRuntimeServer',
     stopRuntimeServer
+  );
+
+  registerCommand(context,
+    'dbuxApplicationsView.clearApplication',
+    () => {
+      allApplications.clear();
+      getProjectManager().practiceSession?.pdp.reset();
+    }
   );
 
   registerCommand(context,
