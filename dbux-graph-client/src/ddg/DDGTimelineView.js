8ad22f5e7b85cab8df02b8789dfe879e4fa014ad
Domiii <Domiii@users.noreply.github.com>
2022-05-14 23:00:13 +0800

    debugging jsplumb

--- dbux-graph-client/src/ddg/DDGTimelineView.js
+++ dbux-graph-client/src/ddg/DDGTimelineView.js
@@ -1,5 +1,6 @@
 import * as jsPlumbBrowserUI from '@jsplumb/browser-ui';
 import { AnchorLocations } from '@jsplumb/common';
+import { BezierConnector } from "@jsplumb/connector-bezier";
 import { compileHtmlElement } from '../util/domUtil';
 import ClientComponentEndpoint from '../componentLib/ClientComponentEndpoint';
 
@@ -28,7 +29,15 @@ export default class DDGTimelineView extends ClientComponentEndpoint {
       this.els.status.textContent = '';
     }
 
+    // TODO: don't re-create jsplumb every time
+    if (this.jsPlumb) {
+      this.jsPlumb.batch(() => {
+        this.clearGraph();
+      });
+    }
+
     // rebuild graph
+
     this.jsPlumb.batch(() => {
       this.clearGraph();
       this.buildGraph();
@@ -58,12 +67,30 @@ export default class DDGTimelineView extends ClientComponentEndpoint {
 
     if (edges) {
       // add edges
+      // console.log('BezierConnector.type', BezierConnector.type);
       for (const edge of edges) {
         const source = this.nodeElMap.get(edge.from);
         const target = this.nodeElMap.get(edge.to);
-        this.jsPlumb.connect({ 
+        this.jsPlumb.connect({
           source,
           target,
+          /**
+           * @see https://docs.jsplumbtoolkit.com/community/lib/connectivity#detaching-connections
+           */
+          detachable: false,
+          connector: {
+            /**
+             * @see https://docs.jsplumbtoolkit.com/community/apidocs/connector-bezier
+             */
+            type: BezierConnector.type,
+            /**
+             * hackfix: always provide `options`, or it will bug out.
+             * @see https://github.com/jsplumb/jsplumb/issues/1129
+             */
+            options: {
+              // curviness: 50
+            }
+          },
           anchor: AnchorLocations.AutoDefault
         });
       }
