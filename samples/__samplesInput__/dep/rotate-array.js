971f0e1c47d6be2861dde129fa789dbb64535a32
Domiii <Domiii@users.noreply.github.com>
2022-05-26 03:56:56 +0800

    add `rotate-array` sample

---
+++ samples/__samplesInput__/dep/rotate-array.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ */
+
+function main(a, delta) {
+  const tmp = a[0];
+  if (delta < 0) {
+    delta = a.length + delta - 1; // make it non-negative
+  }
+  for (let i = a.length - 1; i > 0; --i) {
+    // console.log((i + delta) % a.length, i);
+    a[(i + delta) % a.length] = a[i];
+  }
+  a[delta] = tmp;
+  return a;
+}
+
+console.log(main([1, 2, 3], -1));
