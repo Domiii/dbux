c7ebf957f50be7f858b7eb5f88237817cb014c42
MiccWan <michaelwan0906@gmail.com>
2022-06-20 18:10:40 +0800

    add `dontReset` project config

--- dbux-projects/src/projects/javascript-algorithms/Project.js
+++ dbux-projects/src/projects/javascript-algorithms/Project.js
@@ -18,6 +18,10 @@ export default class JavascriptAlgorithmProject extends Project {
 
   };
 
+  get dontReset() {
+    return true;
+  }
+
   async beforeInstall() {
     // remove husky from package.json
     writeMergePackageJson(this.projectPath, { scripts: { prepare: '' } });
