e2e6eac3b7017096e8366e69e8274dd85cf7955c
Domiii <Domiii@users.noreply.github.com>
2022-07-01 15:14:41 +0800

    chore: clean up (part 4/? - rename 1)

--- scripts/rename-ddg.sh
+++ scripts/rename-ddg.sh
@@ -1,5 +1,7 @@
 # Rename utiltiy file, we used to rename all "ddg" files to "pdg" files.
 # see: https://stackoverflow.com/questions/1086502/rename-multiple-files-based-on-pattern-in-unix/72825427#72825427
+set -e
+set -x
 
 find "C:/Users/domin/code/dbux/dbux-data/src/ddg" -type f -not \( -path "**/node_modules/**" -prune \) -name "*.js" | 
   sed -nE "s/(.*)\/DDG(.*\.js)/& \1\/PDG\2/p" |
