c12247b6a287c251beb550e02b07c690b81dcf1b
Domiii <Domiii@users.noreply.github.com>
2022-07-08 02:28:18 +0800

    work on intro

--- dbux-code/src/dialogs/intro.js
+++ dbux-code/src/dialogs/intro.js
@@ -35,8 +35,16 @@ const intro = {
           node: null // dismiss → will come back again upon next restart
         },
         {
-          text: 'No. Don\'t ask me again.',
-          node: 'end'
+          text: 'I need help with the basics.',
+          node: 'basics'
+        },
+        {
+          text: 'I am interested in the ACG (Asynchronous Call Graph).',
+          node: 'acg'
+        },
+        {
+          text: 'I am interested in the PDG (Program Dependency Graph).',
+          node: 'pdg'
         },
         {
           text: 'No. Don\'t ask me again.',
@@ -46,16 +54,16 @@ const intro = {
     },
     
     basics: {
+      // TODO: merge all this into `start`
       kind: DialogNodeKind.Modal,
-      text: 'TODO',
+      text: 'Here are some resources to get you familiar with the Dbux basics:',
       edges: [
         {
-          text: 'Watch the intro video',
+          text: 'Watch the Intro Video',
           node: 'basics',
           click() {
-
+            return env.openExternal(Uri.parse('https://youtu.be/N9W6rhHMKbA?t=145'));
           }
-          // https://youtu.be/N9W6rhHMKbA?t=145
         },
         {
           text: 'Read the Docs',
@@ -65,15 +73,36 @@ const intro = {
           }
         },
         {
+          text: 'Tutorial',
+          node: 'basics',
+          click() {
+            return env.openExternal(Uri.parse('https://domiii.github.io/dbux/dbux-practice/tutorial'));
+          }
+        },
+        {
           text: 'Ask on Discord',
           async click() {
             return env.openExternal(Uri.parse('https://discord.gg/jWN356W'));
           }
         },
         {
-          text: 'Go back',
+          text: '↩ Go back',
           node: 'start'
         }
+      ]
+    },
+
+    acg: {
+      kind: DialogNodeKind.Modal,
+      text: 'Here are some resources to get you familiar with the Dbux basics:',
+      edges: [
+      ]
+    },
+
+    pdg: {
+      kind: DialogNodeKind.Modal,
+      text: 'Here are some resources to get you familiar with the Dbux basics:',
+      edges: [
       ]
     },
 
