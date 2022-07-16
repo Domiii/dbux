adf34e3f0dcd18f6fd13a222a79497a37653d8b5
Domiii <Domiii@users.noreply.github.com>
2022-07-16 20:54:31 +0800

    fix `cloc`

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -1,4 +1,4 @@
 set -e
 set -x
 
-perl node_modules/cloc/lib/cloc --match-f='.*\.(js|py)$' --fullpath --match-d='(dbux-[^/]+|analysis|samples/(__samplesInput__|case-studies))' --not-match-d='(dbux_projects|node_modules|dist|project_sandbox)$' --not-match-f='.*\.inst\..*'  --by-file .
+perl node_modules/cloc/lib/cloc --match-f='.*\.(js|py)$' --fullpath --match-d='(dbux-[^/]+|analysis|samples/(__samplesInput__|case-studies))' --not-match-d='(dbux_projects|node_modules|dist|project_sandbox)$' --not-match-f='.*\.inst\..*|.*\.dbux\..*|javascript[-]algorithms[-]all\.js'  --by-file .
