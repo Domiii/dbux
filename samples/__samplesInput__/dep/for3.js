9bdf9f81eba59b083985a3aeb40e242f79cd8481
Domiii <Domiii@users.noreply.github.com>
2022-07-30 14:41:41 +0800

    sample

--- samples/__samplesInput__/dep/for3.js
+++ samples/__samplesInput__/dep/for3.js
@@ -1,13 +1,8 @@
 function main(x) {
-  var a = [0];
-  // var n = 3;
-  var n = 2;
-
+  var a = [0], n = 2;
   for (let i = 0; i < n; ++i) {
     a[i] = (a[i - 1] || 0) + x;
   }
-
   return [...a];
 }
-
 main(10, 100);
